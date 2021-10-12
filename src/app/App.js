f234ff819404a3c75a96e40c73317b83fb7cdddc
Maksym_Shveda <maksim1shveda@gmail.com>
2021-10-12 20:05:54 +0300

    start!

---
+++ src/app/App.js
@@ -0,0 +1,22 @@
+
+import './styles.css';
+
+function App() {
+  return (
+   <div className="game">
+     <div className="board">
+       <button className="square"></button>
+       <button className="square"></button>
+       <button className="square"></button>
+       <button className="square"></button>
+       <button className="square"></button>
+       <button className="square"></button>
+       <button className="square"></button>
+       <button className="square"></button>
+       <button className="square"></button>
+     </div>
+   </div>
+  );
+}
+
+export default App;
